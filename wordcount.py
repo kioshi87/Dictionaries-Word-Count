38111168826466c42bd8a828dcfdbd71bcfdc8c5
qfuggett <qfuggett@gmail.com>
2021-03-09 16:27:22 +0000

    create a file path and dictionary syntax

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,15 @@
-# put your code here.
+#open the file
+test_file = open("test.txt") 
+
+#count how many times each space-separated word occurs in 'test_file'
+
+#create the dictionary
+words_count = {}
+counter = 0
+#start looping through each line in the file
+for line in test_file:
+    separated_words = line.split(' ')       #separate each word by a space
+    for word in separated_words:
+        counter += 1
+        words_count[word] = counter
+    print(words_count) 
